e8650fad76201639093620170ab657bbf728e452
Nitish Mhaske <35861324+NitishMhaske@users.noreply.github.com>
2021-02-01 05:53:00 +0530

    Create Day 1: Data Types.java

---
+++ Day 1: Data Types.java
@@ -0,0 +1,16 @@
+Day 1: Data Types
+
+        i2=scan.nextInt();
+        d2=scan.nextDouble();
+        scan.nextLine();
+        s2=scan.nextLine();
+        
+        /* Print the sum of both integer variables on a new line. */
+         System.out.println(i+i2);   
+         
+        /* Print the sum of the double variables on a new line. */
+		      System.out.println(d+d2);
+          
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+          System.out.println(s+s2);
